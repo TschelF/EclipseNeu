803a953ecb74758e0a442d9e5986b49cd31475fa
Tscheliesnig <Tscheliesnig@me.com>
2017-06-06 18:31:22 +0200

    AAAA

---
+++ ZumÜben/src/Bank.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Bank {
+	private ArrayList <Integer> daten;
+	private String name;
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
